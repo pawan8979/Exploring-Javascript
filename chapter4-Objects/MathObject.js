b5318769364f321e86beeebb38605cba0edd7fda
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-05-23 10:26:16 +0530

    Delete chapter4-Objects directory

--- chapter4-Objects/MathObject.js
+++
@@ -1,8 +0,0 @@
-console.log(Math);
-
-let n = 7.6;
-console.log(Math.round(n)); //8
-console.log(Math.floor(n)); //7
-console.log(Math.ceil(n)); //8
-console.log(Math.trunc(n)); //7
-console.log(Math.random());

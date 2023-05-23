869f3093e0ad3aaf3e4046e93d324eff0072a647
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-05-23 10:26:30 +0530

    Delete Chapter5-DOM directory

--- Chapter5-DOM/GettingSettingAttribute.js
+++
@@ -1,9 +0,0 @@
-let youtubeLink = document.querySelector("a");
-//getting/reading the data inside href
-console.log(youtubeLink.getAttribute("href"));
-//set the attribute
-youtubeLink.setAttribute(
-  "href",
-  "https://www.youtube.com/watch?v=lGmRnu--iU8&t=228s"
-);
-youtubeLink.innerText = "Master class of JS";
--- Chapter5-DOM/addingStyle.js
+++
@@ -1,3 +0,0 @@
-let heading = document.querySelector("h1");
-heading.style.color = "Red";
-heading.style.backgroundColor = "Yellow";
